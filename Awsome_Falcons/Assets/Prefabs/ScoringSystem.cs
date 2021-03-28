eaffea007407668e18fb943fc460528cda93fe5a
Khush Soni <khushsoni555@gmail.com>
2021-03-28 16:56:47 +0530

    Final

---
+++ Awsome_Falcons/Assets/Prefabs/ScoringSystem.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoringSystem : MonoBehaviour
+{
+	public GameObject Score;
+	public int theScore;
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag == "SphereCollects")
+		{
+			theScore += 1;
+			Score.GetComponent<Text>().text = "Score: " + theScore;
+		}
+	}
+}
---
+++ Awsome_Falcons/Assets/Scripts/Scores.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Scores : MonoBehaviour
+{
+	public GameObject Score;
+	public int theScore;
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.tag == "SphereCollects")
+		{
+			theScore += 1;
+			Score.GetComponent<Text>().text = "Score: " + theScore;
+		}
+	}
+
+}
